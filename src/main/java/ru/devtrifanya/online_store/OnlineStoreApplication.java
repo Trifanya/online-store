0befe14a73c67c72db6bdb1ca0f9eec65ac8f8d6
Trifanya <80362982+Trifanya@users.noreply.github.com>
2023-11-07 15:37:45 +0300

    Финальный коммит 1.0.

--- src/main/java/ru/devtrifanya/online_store/OnlineStoreApplication.java
+++ src/main/java/ru/devtrifanya/online_store/OnlineStoreApplication.java
@@ -9,7 +9,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
--- src/main/java/ru/devtrifanya/online_store/services/EmailSenderService.java
+++ src/main/java/ru/devtrifanya/online_store/services/EmailSenderService.java
@@ -1,6 +1,7 @@
 package ru.devtrifanya.online_store.services;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
